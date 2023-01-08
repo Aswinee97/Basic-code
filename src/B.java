5c258c7a4bec0c56183e2f74752fd78ad6fc163e
Aswinee97 <imaswinee97@gmail.com>
2023-01-08 10:22:54 +0530

    codesaved

---
+++ src/B.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class B{
+	public static void main(String[] args) {
+		int Arays[]= new int[]{1,5,6,7,5,6,84,9};
+		Arrays.sort(Arays);
+		System.out.println("sorting the arrsys");
+		for (int i = 0; i < Arays.length; i++) {
+			System.out.println(i);
+		}
+		
+	}
+} 
